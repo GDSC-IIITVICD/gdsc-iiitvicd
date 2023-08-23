1b79071bf0ae5e73d05c567293dba63fb6e2e100
kajaveaniruddha <aakajave@gmail.com>
2023-08-23 12:47:34 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Navbar from "./components/Navbar";
-import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Alumni from "./components/Alumni";
+import Volunteer from "./components/Volunteer";
+import Leads from "./components/Leads";
 function App() {
   return (
     <>
-      <Navbar />
+      <div>
+        <BrowserRouter>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/leads" element={<Leads />} />
+            <Route path="/alumni" element={<Alumni />} />
+            <Route path="/volunteer" element={<Volunteer />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
