77bddd69da2d7b8241210cfb657400b10c2a0b02
Yuvraj Chauhan <email.yuvraj.13@gmail.com>
2023-12-01 19:39:29 +0530

    added links to github and linkedin for volunteers

--- src/components/Volunteer/VolunteerDB.jsx
+++ src/components/Volunteer/VolunteerDB.jsx
@@ -3,55 +3,55 @@ export const VolunteerDB = [
         image: "/volunteersPhotos/SaloniShevde.jpg",
         name: "Saloni Shevde",
         domain: "Web Dev Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/salshevde', linkedin: 'https://www.linkedin.com/in/saloni-shevde-886a94282/' }
     },
     {
         image: "/volunteersPhotos/YuvrajChauhan.jpg",
         name: "Yuvraj Chauhan",
         domain: "Web Dev Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/YuvrajChauhan1303', linkedin: 'https://www.linkedin.com/in/yuvraj-chauhan-1a47b4272/' }
     },
     {
         image: "/volunteersPhotos/SujalChauhan.jpg",
         name: "Sujal Chauhan",
         domain: "App Dev Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/CyberFreak01', linkedin: 'https://www.linkedin.com/in/sujal-chauhan-58b361240/' }
     },
     {
         image: "/volunteersPhotos/SanskarKoserwal.jpg",
         name: "Sanskar Koserwal",
         domain: "App Dev Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/sanskar008', linkedin: 'https://www.linkedin.com/in/sanskar-koserwal' }
     },
     {
         image: "/volunteersPhotos/SiddharthNimbalkar.jpg",
         name: "Siddharth Nimbalkar",
         domain: "Management Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/siddharthn183', linkedin: 'https://www.linkedin.com/in/siddharth-nimbalkar-69ba38258' }
     },
     {
         image: "/volunteersPhotos/AyushGangwar.jpg",
         name: "Ayush Gangwar",
         domain: "Management Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/AYUSH-GANGWAR9', linkedin: 'https://www.linkedin.com/in/ayush-gangwar-8a856b272/' }
     },
     {
         image: "/volunteersPhotos/SumedhaBaghmar.jpg",
         name: "Sumedha Baghmar",
         domain: "Management Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/Sumiii15', linkedin: 'https://www.linkedin.com/in/sumedha-baghmar-b016b6261/' }
     },
     {
         image: "/volunteersPhotos/AnayPandey.jpg",
         name: "Anay Pandey",
         domain: "Management Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/anaypandey1504', linkedin: 'https://www.linkedin.com/in/anaykumar-pandey-049336259/' }
     },
     {
         image: "/volunteersPhotos/MokshArora.jpg",
         name: "Moksh Arora",
         domain: "Public Relations Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'http://www.github.com/aroramoksh11', linkedin: 'https://www.linkedin.com/in/aroramoksh/' }
     },
     {
         image: "/volunteersPhotos/VedPrakashMeena.jpg",
@@ -63,36 +63,36 @@ export const VolunteerDB = [
         image: "/volunteersPhotos/TanayPatel.jpg",
         name: "Tanay Patel",
         domain: "UI/UX Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/Tanay-cmd', linkedin: 'https://www.linkedin.com/in/tanay-patel-335418259' }
     },
     {
         image: "/volunteersPhotos/DevrikhJatav.jpg",
         name: "Devrikh Jatav",
         domain: "UI/UX Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/Devrikh', linkedin: 'https://www.linkedin.com/in/devrikh-jatav-6bb512256/' }
     },
     {
         image: "/volunteersPhotos/OmniManwani.jpg",
         name: "Omni Manwani",
         domain: "Women-in Tech Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/omnimanwani', linkedin: 'https://www.linkedin.com/in/omni-manwani' }
     },
     {
         image: "/volunteersPhotos/InaratHussain.jpg",
         name: "Inarat Hussain",
         domain: "Women-in Tech Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/InaratHussain', linkedin: 'https://www.linkedin.com/in/inarat-hussain-612194270/' }
     },
     {
         image: "/volunteersPhotos/SrijanSharma.jpg",
         name: "Srijan Sharma",
         domain: "Technical Writing Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/horiznsky', linkedin: 'https://www.linkedin.com/in/srijan-sharma-10389626a' }
     },
     {
         image: "/volunteersPhotos/RajatThakur.jpg",
         name: "Rajat Kumar Thakur",
         domain: "Open Source Volunteer",
-        contact: { github: '/link', linkedin: '/link' }
+        contact: { github: 'https://github.com/acevaliant', linkedin: 'https://www.linkedin.com/in/rajat-kumar-thakur/' }
     }
 ];
